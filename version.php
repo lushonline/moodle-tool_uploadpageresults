59f587452a59b828d3e62a13a36366182572c58a
lushonline <56685070+lushonline@users.noreply.github.com>
2020-07-03 07:39:59 +0100

    Fixes #3
    Fixes the violations PHPMD reports in Travis Build in tracker.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2020070200; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2020070201; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016052300; // Requires this Moodle version v3.1 see https://docs.moodle.org/dev/Releases.
 $plugin->component = 'tool_uploadpageresults'; // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = ['mod_page' => ANY_VERSION];
