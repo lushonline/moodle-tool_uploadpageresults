2d253a58db3937ab03cc62a3b2443346e9a66c23
lushonline <56685070+lushonline@users.noreply.github.com>
2020-08-17 09:30:40 +0100

    Update version.php
    Add Moodle 3.2+ support back and bump version

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2020081300; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2018051700; // Requires this Moodle version v3.5 see https://docs.moodle.org/dev/Releases.
+$plugin->version   = 2020081700; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2016120500; // Requires this Moodle version v3.2 see https://docs.moodle.org/dev/Releases.
 $plugin->component = 'tool_uploadpageresults'; // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = ['mod_page' => ANY_VERSION];
 $plugin->maturity = MATURITY_STABLE;
