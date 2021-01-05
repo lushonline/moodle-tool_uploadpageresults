0b8afd41c1f7cbbcbe414e31866a30b15cd754d7
lushonline <56685070+lushonline@users.noreply.github.com>
2021-01-05 12:15:25 +0000

    Updated version for MOODLE 3.5 or later only

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2020081700; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2021010500; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016120500; // Requires this Moodle version v3.2 see https://docs.moodle.org/dev/Releases.
 $plugin->component = 'tool_uploadpageresults'; // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = ['mod_page' => ANY_VERSION];
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.5';
+$plugin->release = '1.6';
 
